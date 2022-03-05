960192df59f8886cceb01c34654af26ad2241551
mystique09 <knightkrypto7@gmail.com>
2022-03-05 21:00:43 +0800

    add: hooks

---
+++ src/hooks.ts
@@ -0,0 +1,22 @@
+/** @type {import('@sveltejs/kit').Handle}*/
+export async function handle({ event, resolve }) {
+	event.locals.user = event.request.headers.get('cookie');
+
+	const response = await resolve(event);
+	response.headers.set('Visited', 'bruhh');
+
+	return response;
+}
+
+/** @type {import('@sveltejs/kit').GetSession}*/
+export function getSession(event) {
+	return event.locals.user
+		? {
+				user: {
+					username: event.locals.user.username,
+					email: event.locals.user.email,
+					role: event.locals.user.role
+				}
+		  }
+		: {};
+}
