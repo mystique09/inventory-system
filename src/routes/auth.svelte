ae7072cfc535406dc2adc3e36236c07238ea6af3
mystique09 <knightkrypto7@gmail.com>
2022-03-07 19:57:39 +0800

    add: basic login route

--- src/routes/auth.svelte
+++ src/routes/auth.svelte
@@ -1,2 +1,33 @@
-<h1>Sign In</h1>
-<a href="/">Home</a>
+<script context="module">
+	export async function load({ session }) {
+		if (session) {
+			return {
+				status: 302,
+				redirect: '/dashboard'
+			};
+		}
+		return {
+			props: {}
+		};
+	}
+</script>
+
+<script>
+	async function submitForm() {
+		let res = await fetch('http://localhost:3000/api/auth', {
+			method: 'POST',
+			headers: {
+				'content-type': 'application/json'
+			}
+		});
+
+		const data = await res.json();
+		console.log(data);
+	}
+</script>
+
+<main>
+	<form on:submit|preventDefault={submitForm}>
+		<button type="submit">Login</button>
+	</form>
+</main>
