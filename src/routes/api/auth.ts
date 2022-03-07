471f3e7255adc4a3d8b46c8e3321c0f3a66bbac5
mystique09 <knightkrypto7@gmail.com>
2022-03-07 19:57:22 +0800

    fix: cookie not showing, typo

--- src/routes/api/auth.ts
+++ src/routes/api/auth.ts
@@ -1,23 +1,29 @@
-import { serialize } from "cookie";
+import { serialize } from 'cookie';
 
 export async function post() {
   let data = {
-    "username": "testuser",
-    "email": "testemail@email.com",
-    "role": "Admin"
+    username: 'testuser',
+    email: 'testemail@email.com',
+    role: 'Admin'
   };
 
   return {
     status: 200,
     headers: {
-      "Set-Cookie": serialize("auth", JSON.stringify(data), {
-        path: "/",
+      'Set-Cookie': serialize('auth', JSON.stringify(data), {
+        path: '/',
         maxAge: 60 * 60 * 24 * 7,
-        httpOnly: true,
+        httpOnly: true
       })
     },
     body: {
       data
     }
-  }
+  };
+}
+
+export async function get() {
+  return {
+    body: 'Hello, World!'
+  };
 }
