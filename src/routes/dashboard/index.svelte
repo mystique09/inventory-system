46cb07eb6baaa172317bf336a4eed054d9adff4a
mystique09 <knightkrypto7@gmail.com>
2022-03-07 19:58:35 +0800

    add: basic user authorization

--- src/routes/dashboard/index.svelte
+++ src/routes/dashboard/index.svelte
@@ -1,1 +1,22 @@
+<script context="module">
+	export async function load({ session }) {
+		if (!session) {
+			return {
+				status: 302,
+				redirect: '/auth'
+			};
+		}
+		return {
+			props: {
+				data: 'Welcome to Dashbord'
+			}
+		};
+	}
+</script>
+
+<script>
+	export let data;
+</script>
+
 <h1>Dashbord</h1>
+<p>{data}</p>
--- src/routes/dashboard/inventory.svelte
+++ src/routes/dashboard/inventory.svelte
@@ -1,1 +1,22 @@
+<script context="module">
+	export async function load({ session }) {
+		if (!session) {
+			return {
+				status: 302,
+				redirect: '/auth'
+			};
+		}
+		return {
+			props: {
+				data: 'Welcome to Inventory'
+			}
+		};
+	}
+</script>
+
+<script>
+	export let data;
+</script>
+
 <h1>Inventory</h1>
+<p>{data}</p>
