f11fa6d37b54646557a3a1330349e097e88b4536
mystique09 <knightkrypto7@gmail.com>
2022-03-08 12:44:05 +0800

    add: implemented auth route design

--- src/routes/auth.svelte
+++ src/routes/auth.svelte
@@ -13,6 +13,7 @@
 </script>
 
 <script>
+	let error = "";
 	async function submitForm() {
 		let res = await fetch('http://localhost:3000/api/auth', {
 			method: 'POST',
@@ -24,12 +25,62 @@
 		const data = await res.json();
 		if (data) {
 			window.location.reload();
+			return;
 		}
+		error = data.error;
 	}
 </script>
 
 <main>
 	<form on:submit|preventDefault={submitForm}>
-		<button type="submit">Login</button>
+		<h1>Welcome Back!</h1>
+		<div class="input-wrapper">
+			<label for="username">Username</label>
+			<input type="text" name="username" id="username">
+		</div>
+		<div class="input-wrapper">
+			<label for="password">Password</label>
+			<input type="password" name="password" id="password">
+		</div>
+		<div class="error">
+			{error}
+		</div>
+		<button id="submit" type="submit">Login</button>
+		<div class="info">
+			Doesn't have an account? <a href="/signup">Sign Up</a>
+		</div>
 	</form>
 </main>
+
+<style lang="postcss">
+	main {
+		@apply max-w-4xl mt-auto h-screen flex flex-col justify-center m-auto;
+	}
+	form {
+		@apply p-8 rounded-md flex flex-col items-center justify-center bg-orange-100;
+	}
+	form h1 {
+		@apply font-bold text-orange-400 text-4xl mb-8;
+	}
+	.input-wrapper {
+		@apply flex flex-col items-start justify-start p-2;
+	}
+	.input-wrapper label {
+		@apply font-light pb-2;
+	}
+	.input-wrapper input[type=text], input[type=password] {
+		@apply focus:ring focus:ring-orange-500 hover:border-none outline-none rounded-md py-4 px-12 text-left;
+	}
+	#submit {
+		@apply bg-orange-300 rounded-md px-24 py-4 m-4;
+	}
+	.error {
+		@apply p-2 text-red-700;
+	}
+	.info {
+		@apply pt-4;
+	}
+	a {
+		@apply underline text-orange-500;
+	}
+</style>
