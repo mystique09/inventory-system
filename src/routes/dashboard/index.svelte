dab7d287344ae170bf8e6192c83d8067997966b1
mystique09 <knightkrypto7@gmail.com>
2022-03-10 14:46:35 +0800

    add: dashboard layout

--- src/routes/dashboard/index.svelte
+++ src/routes/dashboard/index.svelte
@@ -26,19 +26,19 @@
 			</ul>
 		</div>
 		<div class="dashboard">
-			<h1>Dashboard</h1>
 			<div class="product-stock-cards">
+				<h2>Dashboard</h2>
 				<div class="card"></div>
 				<div class="card"></div>
 				<div class="card"></div>
 			</div>
-		</div>
-		<div class="chart">
-			<h1>Chart</h1>
-			<div class="chart-cards">
-				<div class="chart-card"></div>
-				<div class="chart-card"></div>
-				<div class="chart-card"></div>
+			<div class="chart">
+				<h2>Chart</h2>
+				<div class="chart-cards">
+					<div class="chart-card"></div>
+					<div class="chart-card"></div>
+					<div class="chart-card"></div>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -46,21 +46,34 @@
 
 <style lang="postcss">
 	main {
-		@apply max-w-4xl mt-auto bg-gray-50;
+		@apply m-auto bg-gray-50 h-screen;
 	}
 	.wrap {
-		@apply grid grid-cols-2;
+		@apply h-full flex flex-col md:flex-row items-start md:justify-between justify-evenly;
 	}
 	.sidebar {
-		@apply w-1/2 h-screen p-4 pt-10;
+		@apply w-full md:w-auto md:h-screen p-4 pt-10 h-1/2;
 	}
 	.sidebar h2 {
-		@apply font-bold text-lg;
+		@apply font-bold text-base md:text-lg;
 	}
 	.sidebar .pages {
 		@apply flex flex-col gap-8 mt-10;
 	}
 	a {
 		@apply hover:underline hover:text-orange-500;
+	}
+	.dashboard {
+		@apply p-10 h-full w-full flex flex-col items-start justify-between;
+	}
+	.dashboard .product-stock-cards {
+		@apply h-1/2;
+	}
+	.chart {
+		@apply h-1/2;
+	}
+	.dashboard h2,
+	 .chart h2 {
+		@apply font-bold text-xl md:text-3xl;
 	}
 </style>
