76259850bd7c049481887580562cddd4cd02bc23
mystique09 <knightkrypto7@gmail.com>
2022-03-18 16:57:40 +0800

    add: stores todo

--- src/stores/product.ts
+++ src/stores/product.ts
@@ -1,3 +1,16 @@
 import { writable, type Writable } from "svelte/store";
 
 export const sample_products: Writable<Array<Product>> = writable([]);
+
+// TODO:
+export function addProduct(payload: Product) {
+    // TODO:
+}
+
+export function updateProduct(payload: Product) {
+    // TODO:
+}
+
+export function deleteProduct(payload: Product) {
+    // TODO:
+}
