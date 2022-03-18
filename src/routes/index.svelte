a9a789b0a344e086cc2cc09342ed1661a24cf270
mystique09 <knightkrypto7@gmail.com>
2022-03-18 18:18:46 +0800

    change: idk, useless ig

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,21 +3,25 @@
 		if (session) {
 			return {
 				status: 301,
-				redirect: "/dashboard"
-			}
+				redirect: '/dashboard'
+			};
 		}
-		return {}
+		return {};
 	}
 </script>
 
 <main>
 	<div class="wrap">
 		<div class="img">
-			<img src="/undraw_real_time_analytics_re_yliv.svg" alt="Data">
+			<img src="/undraw_real_time_analytics_re_yliv.svg" alt="Data" />
 		</div>
 		<div class="content">
 			<h1>Inventory System</h1>
-			<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam atque quidem cum mollitia laudantium fugit dignissimos saepe quia ad, modi deserunt quod amet velit maxime? Fuga quasi aspernatur quisquam exercitationem?</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam atque quidem cum mollitia
+				laudantium fugit dignissimos saepe quia ad, modi deserunt quod amet velit maxime? Fuga quasi
+				aspernatur quisquam exercitationem?
+			</p>
 		</div>
 		<a class="signup-btn" href="/signup">Create your own account</a>
 	</div>
