01274c3b95eaab672c932e430bf55af8f9018b58
mystique09 <knightkrypto7@gmail.com>
2022-03-18 18:20:18 +0800

    add: sidebar component

--- src/lib/components/dashboard_sidebar.svelte
+++ src/lib/components/dashboard_sidebar.svelte
@@ -1,22 +1,22 @@
 <script>
-    let toggle = false;
-    function toggleSidebar() {
-        toggle = !toggle;
-        console.log(toggle)
-    }
+	let toggle = false;
+	function toggleSidebar() {
+		toggle = !toggle;
+		console.log(toggle);
+	}
 </script>
 
 <div class="sidebar">
-    <h2 on:click={toggleSidebar}>Navigation</h2>
-    <ul class="pages" class:hidden={!toggle}>
-        <li><a href="/">Dashboard</a></li>
-        <li><a href="/dashboard/location">Location</a></li>
-        <li><a href="/dashboard/inventory">Inventory</a></li>
-    </ul>
+	<h2 on:click={toggleSidebar}>Navigation</h2>
+	<ul class="pages" class:hidden={!toggle}>
+		<li><a href="/">Dashboard</a></li>
+		<li><a href="/dashboard/location">Location</a></li>
+		<li><a href="/dashboard/inventory">Inventory</a></li>
+	</ul>
 </div>
 
 <style lang="postcss">
-    .sidebar {
+	.sidebar {
 		@apply w-full md:w-auto md:h-auto p-4 pt-10;
 	}
 	.sidebar h2 {
