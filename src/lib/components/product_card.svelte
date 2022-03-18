52972793c8c03770356f188d2bc468f5f1a1a705
mystique09 <knightkrypto7@gmail.com>
2022-03-18 18:20:34 +0800

    add: product component

--- src/lib/components/product_card.svelte
+++ src/lib/components/product_card.svelte
@@ -1,8 +1,26 @@
-<div class="card">
+<script lang="ts">
+	export let name: string;
+	export let brand: string;
+	export let category: string;
+	export let price: number;
+	export let stock: number;
+</script>
 
-</div>
+<tr>
+	<td>{name}</td>
+	<td>{category}</td>
+	<td>{brand}</td>
+	<td>{price}</td>
+	<td>{stock}</td>
+	<td>{stock !== 0 ? 'In Stock' : 'Out of Stock'}</td>
+	<td class="option">
+		<button type="button">Delete</button>
+		<button>Edit</button>
+	</td>
+</tr>
 
 <style lang="postcss">
-
+	td {
+		@apply text-xs font-light border-collapse border border-slate-500 px-8;
+	}
 </style>
-
