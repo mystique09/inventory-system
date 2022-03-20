66c0dce531054f891387699c46748dc39fe810a8
mystique09 <knightkrypto7@gmail.com>
2022-03-20 16:42:10 +0800

    fix: idk

--- src/hooks.ts
+++ src/hooks.ts
@@ -2,14 +2,14 @@ import { parse } from 'cookie';
 
 /** @type {import('@sveltejs/kit').Handle}*/
 export async function handle({ event, resolve }) {
-	const cookie = parse(event.request.headers.get('cookie') || '');
-	event.locals.user = cookie.auth ? cookie.auth : null;
+  const cookie = parse(event.request.headers.get('cookie') || '');
+  event.locals.user = cookie.auth ? cookie.auth : null;
 
-	const response = await resolve(event);
-	return response;
+  const response = await resolve(event);
+  return response;
 }
 
 /** @type {import('@sveltejs/kit').GetSession}*/
 export function getSession(event) {
-	return event.locals.user;
+  return event.locals.user;
 }
